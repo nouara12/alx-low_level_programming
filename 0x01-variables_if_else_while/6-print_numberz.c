2812465ed069972c2b851094837d78ad3afb2f90
Sara NOUAR <saranrfb@gmail.com>
2023-04-21 10:38:29 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	int i;
+	int d;
 
-	for (i = 0; i < 10; i++)
-	printf("%d", i);
-	printf("\n");
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
 	return (0);
 }

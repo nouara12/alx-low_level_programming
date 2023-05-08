f117b7e57dcc4ffe933ab4ecfac9b0f3a24b45dc
Sara NOUAR <saranrfb@gmail.com>
2023-05-08 14:46:17 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,19 +8,23 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int a = 0;
-	int b = 0;
+	int a, b;
 	char *p;
 
+	a = 0;
 	while (s[a] != '\0')
+	{
+		b = 0;
 		while (accept[b] != '\0')
 		{
 			if (accept[b] == s[a])
-			{ p = &s[a];
+			{
+				p = &s[a];
 				return (p);
 			}
 			b++;
 		}
 	a++;
+	}
 	return (0);
 }

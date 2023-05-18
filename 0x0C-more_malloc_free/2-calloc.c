a40f3d44a50bc99fd4530b8a5033d95bfd910e64
Sara NOUAR <saranrfb@gmail.com>
2023-05-18 11:03:24 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -34,7 +34,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	p = malloc(sizeof(int) * nmemb);
 
-	if (p == 0)
+	if (p == NULL)
 		return (NULL);
 	_memset(p, 0, sizeof(int) * nmemb);
 

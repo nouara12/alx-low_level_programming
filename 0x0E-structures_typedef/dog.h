741604de125ab671aeca0415dcd9d61b5da24750
Sara NOUAR <saranrfb@gmail.com>
2023-05-22 11:38:51 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _dog_h
+#define _dog_h
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: dog's informations are struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

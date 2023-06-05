b7da532f757845f3b66e3764f485607b71b9a57f
Sara NOUAR <saranrfb@gmail.com>
2023-06-05 15:41:42 -0700

    task13

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: first node of list
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint_safe - frees a linked list
+ * @h: first node of list
+ * Return: number of elements in the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t l = 0;
+	int a;
+	listint_t *t;
+
+	if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		a = *h - (*h)->next;
+		if (a > 0)
+		{
+			t = (*h)->next;
+			free(*h);
+			*h = t;
+			l++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			l++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (l);
+}
